625dba8320a631b5c8956a7584a8fe6222741b76
chomik <to.chomik@gmail.com>
2014-11-03 21:02:47 +0100

    Make awesome not honoring size hints

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -473,7 +473,9 @@ awful.rules.rules = {
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      keys = clientkeys,
-                     buttons = clientbuttons } },
+                     buttons = clientbuttons,
+                     size_hints_honor = false
+                 } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
     { rule = { class = "gimp" },
